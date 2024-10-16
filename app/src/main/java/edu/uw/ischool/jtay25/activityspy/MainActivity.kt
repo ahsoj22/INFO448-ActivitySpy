abd3d0fd0bc0a6dff73d4029cbd81a3400bc268e
Joshua Taylor <joshuaetaylor231@gmail.com>
2024-10-16 16:02:25 -0700

    finished assignment

---
+++ app/src/main/java/edu/uw/ischool/jtay25/activityspy/MainActivity.kt
@@ -0,0 +1,57 @@
+package edu.uw.ischool.jtay25.activityspy
+
+import android.os.Bundle
+import android.util.Log
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    companion object {
+        private const val TAG = "MainActivity"
+    }
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        Log.i(TAG, "onCreate event fired")
+        savedInstanceState?.let {
+            Log.i(TAG, "onCreate parameters: $it")
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.i(TAG, "onStart event fired")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.i(TAG, "onResume event fired")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.i(TAG, "onPause event fired")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.i(TAG, "onStop event fired")
+    }
+
+    override fun onDestroy(){
+        super.onDestroy()
+        Log.i(TAG, "OnDestroy event fired")
+        Log.e(TAG, "We're going down, Captain! -- Scotty, Star Trek")
+    }
+}
